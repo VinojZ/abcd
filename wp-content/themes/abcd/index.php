4c007a8219d71c5af8ed1261a5ec9317f46e658d
Vinoj <vinojz@gmail.com>
2014-05-20 07:04:01 -0700

    Blog: Image added as background for each post

--- wp-content/themes/abcd/index.php
+++ wp-content/themes/abcd/index.php
@@ -1,34 +1,41 @@
-<?php get_header(); ?>	
+<?php get_header(); ?>
 <section class="intro">
 	<h1>The ABCD Institute</h1>
 	<aside>Duis et bibendum nulla. Ut at arcu sapien. Nulla accumsan malesuada faucibus. Sed ut dui vel nisl adipiscing faucibus ac vitae purus. Etiam porta sapien non ante egestas ultrices. Sed sed bibendum odio. In ut neque tellus. Etiam est lacus, semper id imperdiet sed, tincidunt bibendum est.Suspendisse arcu justo, mattis a dignissim vel, malesuada id nisl.</aside>
 </section>
 <section class="latest-post">
-	<article>
+	<?php
+		$args = array( 'posts_per_page' => 1);
+		$myposts = get_posts( $args );
+		foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
 		<?php
-			$args = array( 'posts_per_page' => 1);
-			$myposts = get_posts( $args );
-			foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+			$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array(360,360 ), false, '' );
+		?>
+		<article style="background-image: url(<?php echo $src[0]; ?> );">
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<p><?php the_excerpt(); ?></p>
+			<a href="<?php the_permalink(); ?>" class="read-more" title="<?php the_title(); ?>">Read more</a>
+		</article>
+		<?php endforeach; 
+			wp_reset_postdata();
+	?>
+	<?php global $post; ?>
+</section>
+<section class="more-reading">
+	<?php
+		$args = array( 'posts_per_page' => 6, 'offset'=> 2);
+		$myposts = get_posts( $args );
+		foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+		<?php
+			$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array(360,360 ), false, '' );
+		?>
+			<article style="background-image: url(<?php echo $src[0]; ?> );">
 				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 				<p><?php the_excerpt(); ?></p>
 				<a href="<?php the_permalink(); ?>" class="read-more" title="<?php the_title(); ?>">Read more</a>
-			<?php endforeach; 
-				wp_reset_postdata();
-		?>
-	</article>
-</section>
-<section class="more-reading">
-		<?php
-			$args = array( 'posts_per_page' => 6, 'offset'=> 2);
-			$myposts = get_posts( $args );
-			foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-				<article>
-					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-					<p><?php the_excerpt(); ?></p>
-					<a href="<?php the_permalink(); ?>" class="read-more" title="<?php the_title(); ?>">Read more</a>
-				</article>
-			<?php endforeach; 
-				wp_reset_postdata();
-		?>
+			</article>	
+		<?php endforeach; 
+			wp_reset_postdata();
+	?>
 </section>
 <?php get_footer(); ?>
