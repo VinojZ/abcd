d53ad91b3e5fd0bed82855240685d83cba2abb9d
VinojZ <vinojz@gmail.com>
2014-05-23 10:31:51 -0700

    HP: Added scrolling navigation to various sections

--- wp-content/themes/abcd/page-homepage.php
+++ wp-content/themes/abcd/page-homepage.php
@@ -184,6 +184,20 @@ Template Name: Homepage
 				$('.tab-content').removeClass().addClass('row');
 			}
 
+			$('a[href*=#]:not([href=#])').click(function() {
+    			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      				var target = $(this.hash);
+      				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      				if (target.length) {
+        				$('html,body').animate({
+          				scrollTop: target.offset().top
+        			}, 1000);
+        			return false;
+      			}
+      		}
+
+  		});
+
 		})(jQuery);
 	</script>
 
