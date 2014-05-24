9ded8f65104888ab2215169b8b52cb771515ef2c
tgchrisporter <chrisporter@nowthatsgenius.com>
2014-05-24 15:18:27 -0700

    Styled industries and services section.

--- wp-content/themes/abcd/page-homepage.php
+++ wp-content/themes/abcd/page-homepage.php
@@ -15,7 +15,6 @@ Template Name: Homepage
 		<div class="mailing-list">
 			<h2>Join our Mailing List</h2>
 			<?php if (function_exists('wp_email_capture_form')) { wp_email_capture_form(); } ?>
-			<?//php get_sidebar(); // sidebar 1 -- use this to pull in a mailing list plugin ?>
 		</div>
 		<div class="beliefs">
 			<ul class="nav nav-pills" id="tabs">
@@ -27,25 +26,25 @@ Template Name: Homepage
 				<div class="people tab-pane active" id="people">
 					<h3>People</h3>
 					<p>People are lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam iaculis lectus massa, ac varius neque laoreet ac. </p>
-					<a class="learn-more" href="#">Learn More</a>
+					<a class="btn btn-blue" href="#">Learn More</a>
 				</div>
 				
 				<div class="performance tab-pane" id="performance">
 					<h3>Performance</h3>
 					<p>Performance is adipiscing elit. Nullam iaculis lectus massa, ac varius neque laoreet ac. Morbi nulla diam, fringilla vitae porta quis, egestas ac libero.</p>
-					<a class="learn-more" href="#">Learn More</a> 
+					<a class="btn btn-blue" href="#">Learn More</a> 
 				</div>
 				<div class="product tab-pane" id="product">
 					<h3>Product</h3>
 					<p>Product was onsectetur adipiscing elit. Nullam iaculis lectus massa, ac varius neque laoreet ac. Morbi nulla diam, fringilla vitae porta quis, egestas ac libero.</p>
-					<a class="learn-more" href="#">Learn More</a> 
+					<a class="btn btn-blue" href="#">Learn More</a> 
 				</div>
 			</div><!-- end .tab-content -->
 		</div><!-- end .beliefs -->
 	</section><!-- end .company-culture -->
-	<section class="meet-the-team" id="meet-the-leadership">
+	<section class="meettheteam" id="meet-the-leadership">
 		<div class="description">
-			<h2>Meet the Team</h2>
+			<h2 class="meettheteam-title">Meet the Team</h2>
 			<p>Our leadership team is comprised of talented and highly respected industry leaders across various disciplines. At ABCD & Company, we believe in leaders who take a hands-on approach and have the pulse of our clients and growing trends in our industries.</p>
         </div><!-- end .meet-the-leadership -->
 		<div class="member">
@@ -99,12 +98,12 @@ Template Name: Homepage
 		<div class="call-to-action">
 			<h5>We want the best</h5>
 			<h6>Great people make a great company</h6>
-			<a class="button" href="#">Join The Team</a>
+			<a class="btn btn-green" href="#">Join The Team</a>
 		</div><!-- end .cta -->               
 	</section><!-- end .meet-the-team -->
 	<section class="industries" id="industries">
 		<div class="description">
-			<h2>Industries</h2>
+			<h2 class="industries-title">Industries</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ornare arcu venenatis felis condimentum, id commodo nisl malesuada. Proin varius lectus vitae odio mattis feugiat. Ut quis massa neque. Aenean neque turpis, dignissim non condimentum sed.</p>
 		</div>
 		<div class="industry">
@@ -182,55 +181,90 @@ Template Name: Homepage
 		<div class="call-to-action">
 			<h5>Lorem ipsum dolor sit</h5>
 			<h6>Quisque ornare arcu venenatis felis condimentum</h6>
-			<a class="button" href="#">Call to Action</a>
+			<a class="btn btn-green" href="#">Call to Action</a>
 		</div><!-- end .cta -->
 	</section><!-- end .industries -->
 	<section class="services" id="services">
-			<div class="description">
-			<h2>Services</h2>
+		<div class="description">
+			<h2 class="typeSerifHeader typeSerifHeaderWhite">Services</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ornare arcu venenatis felis condimentum, id commodo nisl malesuada. Proin varius lectus vitae odio mattis feugiat. Ut quis massa neque. Aenean neque turpis, dignissim non condimentum sed.</p>
 		</div>
 		<div class="service">
-			<img src="http://placehold.it/330x268">
-			<p>Lorem Ipsum Dolor Sit Amet</p>
-			<p>Consectetur adipiscing</p>
+			<i class="icon icon-services icon-services-stakeholderbranding">Stakeholder Branding</i>
+			<h3 class="service-title">Stakeholder Branding</h3>
+			<ol class="service-list">
+				<li>Strategic Branding Events</li>
+				<li>Image Consulting</li>
+				<li>Fundraising Events</li>
+				<li>Private Events</li>
+			</ol>
 		</div>
 		<div class="service">
-			<img src="http://placehold.it/330x268">
-			<p>Lorem Ipsum Dolor Sit Amet</p>
-			<p>Consectetur adipiscing</p>
+			<i class="icon icon-services icon-services-projectmanagement">Stakeholder Branding</i>
+			<h3 class="service-title">Project Management</h3>
+			<ol class="service-list">
+				<li>Strategic Branding Events</li>
+				<li>Image Consulting</li>
+				<li>Fundraising Events</li>
+				<li>Private Events</li>
+			</ol>
 		</div>
 		<div class="service">
-			<img src="http://placehold.it/330x268">
-			<p>Lorem Ipsum Dolor Sit Amet</p>
-			<p>Consectetur adipiscing</p>
+			<i class="icon icon-services icon-services-businesscharterservices">Stakeholder Branding</i>
+			<h3 class="service-title">Business Charter Services</h3>
+			<ol class="service-list">
+				<li>Strategic Branding Events</li>
+				<li>Image Consulting</li>
+				<li>Fundraising Events</li>
+				<li>Private Events</li>
+			</ol>
 		</div>
 		<div class="service">
-			<img src="http://placehold.it/330x268">
-			<p>Lorem Ipsum Dolor Sit Amet</p>
-			<p>Consectetur adipiscing</p>
+			<i class="icon icon-services icon-services-businessdevservices">Stakeholder Branding</i>
+			<h3 class="service-title">Business Development Services</h3>
+			<ol class="service-list">
+				<li>Strategic Branding Events</li>
+				<li>Image Consulting</li>
+				<li>Fundraising Events</li>
+				<li>Private Events</li>
+			</ol>
 		</div>
 		<div class="service">
-			<img src="http://placehold.it/330x268">
-			<p>Lorem Ipsum Dolor Sit Amet</p>
-			<p>Consectetur adipiscing</p>
+			<i class="icon icon-services icon-services-changemanagement">Stakeholder Branding</i>
+			<h3 class="service-title">Change Management</h3>
+			<ol class="service-list">
+				<li>Strategic Branding Events</li>
+				<li>Image Consulting</li>
+				<li>Fundraising Events</li>
+				<li>Private Events</li>
+			</ol>
 		</div>
 		<div class="service">
-			<img src="http://placehold.it/330x268">
-			<p>Lorem Ipsum Dolor Sit Amet</p>
-			<p>Consectetur adipiscing</p>
+			<i class="icon icon-services icon-services-humanresourceconsultant">Stakeholder Branding</i>
+			<h3 class="service-title">Human Resource Consultant</h3>
+			<ol class="service-list">
+				<li>Strategic Branding Events</li>
+				<li>Image Consulting</li>
+				<li>Fundraising Events</li>
+				<li>Private Events</li>
+			</ol>
 		</div>
 		<div class="service">
-			<img src="http://placehold.it/330x268">
-			<p>Lorem Ipsum Dolor Sit Amet</p>
-			<p>Consectetur adipiscing</p>
+			<i class="icon icon-services icon-services-corporatetraining">Stakeholder Branding</i>
+			<h3 class="service-title">Corporate Training and Development</h3>
+			<ol class="service-list">
+				<li>Strategic Branding Events</li>
+				<li>Image Consulting</li>
+				<li>Fundraising Events</li>
+				<li>Private Events</li>
+			</ol>
 		</div>
 	</section><!-- end .services -->
 	<section class="get-started" id="get-started">
-		<div class="call-to-action">
+		<div class="call-to-action call-to-action-solo">
 			<h5>Lorem ipsum dolor sit</h5>
 			<h6>Quisque ornare arcu venenatis felis condimentum</h6>
-			<a class="button" href="#">Call to Action</a>
+			<a class="btn btn-blue" href="#">Call to Action</a>
 		</div><!-- end .cta -->
 	</section><!-- end .get-started -->
 	<script>
