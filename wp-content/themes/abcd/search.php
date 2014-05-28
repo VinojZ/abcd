8cad4d7ba8e2190c03e7e136093d0d8c7cf3379f
Vinoj <vinojz@gmail.com>
2014-05-27 21:45:58 -0700

    Search: Google Custom Search
    * Google Custom Search implemented with dev account
    * Fixed odd bug with search button image where background was not
    displaying
    * Fixed bug where selected option container was breaking

--- wp-content/themes/abcd/search.php
+++ wp-content/themes/abcd/search.php
@@ -10,16 +10,16 @@ Template Name: Search
 
 	<div class="engine">
 		<script>
-			  (function() {
-			    var cx = '009537608922408596453:rzqggn28qgk';
-			    var gcse = document.createElement('script');
-			    gcse.type = 'text/javascript';
-			    gcse.async = true;
-			    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-			        '//www.google.com/cse/cse.js?cx=' + cx;
-			    var s = document.getElementsByTagName('script')[0];
-			    s.parentNode.insertBefore(gcse, s);
-			  })();
+		  (function() {
+		    var cx = '009537608922408596453:rzqggn28qgk';
+		    var gcse = document.createElement('script');
+		    gcse.type = 'text/javascript';
+		    gcse.async = true;
+		    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+		        '//www.google.com/cse/cse.js?cx=' + cx;
+		    var s = document.getElementsByTagName('script')[0];
+		    s.parentNode.insertBefore(gcse, s);
+		  })();
 		</script>
 		<gcse:search></gcse:search>
 	</div><!-- end .engine -->
