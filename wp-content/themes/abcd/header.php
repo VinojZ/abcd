848e3c2b8fef99c5dc9bd9943754a71432807cf4
VinojZ <vinojz@gmail.com>
2014-05-30 15:41:08 -0700

    Title: Using WP function for page title

--- wp-content/themes/abcd/header.php
+++ wp-content/themes/abcd/header.php
@@ -9,7 +9,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title><?php wp_title( '| ABCD Company', true, 'right' ); ?></title>	
+		<title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo('name')?></title>	
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				
 		<!-- media-queries.js (fallback) -->
