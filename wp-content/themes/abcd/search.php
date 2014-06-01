935298d56a17361a7a0dcb50fc934ef895e406d8
Vinoj <vinojz@gmail.com>
2014-05-31 17:16:25 -0700

    Search: Updated with ABCD company creds

--- wp-content/themes/abcd/search.php
+++ wp-content/themes/abcd/search.php
@@ -5,26 +5,27 @@ Template Name: Search
 ?>
 
 <?php get_header(); ?>
+<div class="container">
+	<section class="google-custom-search">
 
-<section class="google-custom-search">
+		<div class="engine">
+			<script>
+			  (function() {
+			    var cx = '015226466563954189157:t0s6vhvyq9k';
+			    var gcse = document.createElement('script');
+			    gcse.type = 'text/javascript';
+			    gcse.async = true;
+			    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+			        '//www.google.com/cse/cse.js?cx=' + cx;
+			    var s = document.getElementsByTagName('script')[0];
+			    s.parentNode.insertBefore(gcse, s);
+			  })();
+			</script>
+			<gcse:search></gcse:search>
+		</div><!-- end .engine -->
 
-	<div class="engine">
-		<script>
-		  (function() {
-		    var cx = '009537608922408596453:rzqggn28qgk';
-		    var gcse = document.createElement('script');
-		    gcse.type = 'text/javascript';
-		    gcse.async = true;
-		    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-		        '//www.google.com/cse/cse.js?cx=' + cx;
-		    var s = document.getElementsByTagName('script')[0];
-		    s.parentNode.insertBefore(gcse, s);
-		  })();
-		</script>
-		<gcse:search></gcse:search>
-	</div><!-- end .engine -->
-
-</section>
+	</section>
+</div><!-- end .container -->
 
 
 <?php get_footer(); ?>
